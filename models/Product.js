b1eec271bb962f2952b56ec1e7307efc1e7e6a73
chriscoulbeck <chriscoulbeck7@gmail.com>
2020-10-15 22:45:31 +1300

    fix: Save product.js from prev commit

--- models/Product.js
+++ models/Product.js
@@ -4,10 +4,6 @@ const ProductsSchema = new mongoose.Schema({
     title: {type: String, required: true},
     price:{type: String, required: true},
     username: {type: String, required: true},
-<<<<<<< HEAD
-=======
-    details:{type: String, required: true},
->>>>>>> 528c1b24b410d682eb49dabeafb342952fc42c18
     description: {type: String, required: true},
 
 });
